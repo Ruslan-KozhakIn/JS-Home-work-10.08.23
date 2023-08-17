168170fd865971c22bbe83586670669be99559f7
Ruslan-KozhakIn <ruslangeraklyon@gmail.com>
2023-08-17 04:19:48 +0300

    finish first task

--- Home work/hw-01.js
+++ Home work/hw-01.js
@@ -1,5 +1,10 @@
+//  #1 Solution
 let userLogin = prompt('Enter your login!');
 let userEmail = prompt('Enter your email!');
 let userPassword = prompt('Enter your password!');
 
-alert(`Dear user, your login is ${userLogin}, your email is ${userEmail}, your password is ${userPassword}`);
+alert(`Dear ${userLogin}, your email is ${userEmail}, your password is ${userPassword}.`);
+
+// alert("Dear " + userLogin + "," + " your email is " + userEmail + "," + " your password is " + userPassword + ".");
+
+
