6cc5f1ce8d6ded0baa267eb7d3c7c05cefd5178a
Ruslan-KozhakIn <ruslangeraklyon@gmail.com>
2023-08-17 03:14:11 +0300

    first solution

---
+++ Home work/hw-01.js
@@ -0,0 +1,5 @@
+let userLogin = prompt('Enter your login!');
+let userEmail = prompt('Enter your email!');
+let userPassword = prompt('Enter your password!');
+
+alert(`Dear user, your login is ${userLogin}, your email is ${userEmail}, your password is ${userPassword}`);
